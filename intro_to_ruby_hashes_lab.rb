af0ded546a1e382b16baf9824374a11abd333f4c
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-14 14:07:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-		monopoly = {:railroads => {:pieces => 4, :names => {}, :rent_in_dollars => { }}
+		monopoly = {:railroads => {:pieces => 4, :names => {}, :rent_in_dollars => []}
 	  
 	}
 
